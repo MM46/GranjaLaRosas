9c7c33f3f227532a1eb54d025065ab7079661a70
mauriciogm97 <mauriciogm97@hotmail.com>
2020-04-01 17:51:18 -0600

    login endpoints

---
+++ functions/controllers/sessions.js
@@ -0,0 +1,17 @@
+const createUser = function (req, res) {
+
+}
+
+const login = function (req, res) {
+
+}
+
+const logout = function (req, res) {
+
+}
+
+module.exports = {
+  createUser: createUser,
+  login: login,
+  logout: logout
+}
--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,13 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const router = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+exports.app = functions.https.onRequest(app);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
