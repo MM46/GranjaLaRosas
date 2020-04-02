e5cc32574ea921d704256e858cdd7d1a198abad0
mauriciogm97 <mauriciogm97@hotmail.com>
2020-04-01 18:07:16 -0600

    restructure files

--- functions/index.js
+++ functions/index.js
@@ -8,10 +8,3 @@ app.use(express.json());
 app.use(router);
 
 exports.app = functions.https.onRequest(app);
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
--- functions/routes.js
+++ functions/routes.js
@@ -1,78 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const admin = require('firebase-admin');
-const firebase = admin.initializeApp();
-const db = firebase.firestore();
+const sessions = require('./controllers/sessions.js')
 
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-router.post('/createUser', (req, res) => {
-  const body = req.body;
-  db.collection('users').doc(body.username).get().then(function (doc) {
-    if (doc.exists) {
-      return res.status(500).send("El usuario ya existe");
-    } else {
-      bcrypt.hash(body.pass, 8).then(function (hashed_pass) {
-        user = {
-          'name': body.name,
-          'username': body.username,
-          'pass': hashed_pass,
-          'tokens': []
-        }
-        db.collection('users').doc(body.username).set(user);
-        return res.send(user);
-      }).catch(function (error) {
-        return res.status(400).send("Contraseña invalida");
-      });
-    }
-  }).catch(function (error) {
-    return res.status(400).send(error);
-  });
-});
-
-router.post('/login', (req, res) => {
-  const body = req.body;
-  db.collection('users').doc(body.username).get().then(function (doc) {
-    if (doc.exists) {
-      const user = doc.data();
-      bcrypt.compare(body.pass, user.pass).then(function (match) {
-        if (match) {
-          // TODO: Ocultar el screto en otro archivo.
-          const token = jwt.sign(user, "SECRETOPENDIENTE", {
-            expiresIn: '7 days'
-          });
-          user.tokens.push(token);
-          db.collection('users').doc(user.username).set(user);
-          return res.send(token);
-        } else {
-          return res.status(500).send("Credenciales inválidas");
-        }
-      }).catch(function (error) {
-        return res.status(500).send("Credenciales inválidas");
-      });
-    } else {
-      return res.status(500).send("Credenciales inválidas");
-    }
-  });
-});
-
-router.post('/logout', (req, res) => {
-  const body = req.body;
-  db.collection('users').doc(body.username).get().then(function (doc) {
-    const user = doc.data();
-    const index = user.tokens.indexOf(body.token);
-    if (index != -1) {
-      user.tokens.splice(index, 1);
-      db.collection('users').doc(user.username).set(user);
-      return res.send(user);
-    } else {
-      return res.status(400).send("Token not found");
-    }
-  }).catch(function (error) {
-    return res.status(400).send("User not found");
-  });
-});
+router.post('/createUser', sessions.createUser);
+router.post('/login', sessions.login);
+router.post('/logout', sessions.logout);
 
 module.exports = router;
