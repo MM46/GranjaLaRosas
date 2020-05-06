1237880e1b1530ad8e3524aa468408e64bd1d245
mauriciogm97 <mauriciogm97@hotmail.com>
2020-05-06 01:18:05 -0500

    Refactor tests

--- functions/test/concept_reports.test.js
+++ functions/test/concept_reports.test.js
@@ -1,7 +1,7 @@
 const request = require('request-promise');
 const assert = require('assert');
 
-function addConcept(token, data) {
+function addConcept(token, body) {
   return request({
     method: 'POST',
     json: true,
@@ -10,16 +10,38 @@ function addConcept(token, data) {
       'Authorization': token
     },
     uri: 'http://localhost:5000/addConcept',
-    body: {
-      'date': data.date,
-      'description': data.description,
-      'cost': data.cost,
-      'earning': data.earning
-    }
+    body: body
   }, function (_, _, body) { });
 }
 
-function getConceptsReport_test(token, body, expected_res) {
+async function addConcepts(token) {
+  await addConcept(token, {
+    'date': 20200501,
+    'description': 'comprar semilla frijol',
+    'cost': 1000,
+    'earning': false
+  });
+  await addConcept(token, {
+    'date': 20200502,
+    'description': 'comprar semilla chicharo',
+    'cost': 2000,
+    'earning': false
+  });
+  await addConcept(token, {
+    'date': 20200503,
+    'description': 'venta de frijol',
+    'cost': 3000,
+    'earning': true
+  })
+  return addConcept(token, {
+    'date': 20200504,
+    'description': 'venta de chicharo',
+    'cost': 4000,
+    'earning': true
+  })
+}
+
+function getConceptsReport_test(token) {
   return request({
     method: 'GET',
     json: true,
@@ -29,80 +51,11 @@ function getConceptsReport_test(token, body, expected_res) {
     },
     uri: 'http://localhost:5000/getConceptsReport',
     body: {
-      'from': body.from,
-      'to': body.to
-    }
-  }, function (err, res, body) {
-    assert(JSON.stringify(body).localeCompare(JSON.stringify(expected_res)) == 0);
-  });
-}
-
-function getEarningsReport_test(token, body, expected_res) {
-  return request({
-    method: 'GET',
-    json: true,
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': token
-    },
-    uri: 'http://localhost:5000/getEarningsReport',
-    body: {
-      'from': body.from,
-      'to': body.to
-    }
-  }, function (err, res, body) {
-    assert(JSON.stringify(body).localeCompare(JSON.stringify(expected_res)) == 0);
-  });
-}
-
-function getExpensesReport_test(token, body, expected_res) {
-  return request({
-    method: 'GET',
-    json: true,
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': token
-    },
-    uri: 'http://localhost:5000/getExpensesReport',
-    body: {
-      'from': body.from,
-      'to': body.to
-    }
-  }, function (err, res, body) {
-    assert(JSON.stringify(body).localeCompare(JSON.stringify(expected_res)) == 0);
-  });
-}
-
-function runTests(admin_token) {
-  return new Promise(async function (resolve, reject) {
-    await addConcept(admin_token, {
-      'date': 20200501,
-      'description': 'comprar semilla frijol',
-      'cost': 1000,
-      'earning': false
-    });
-    await addConcept(admin_token, {
-      'date': 20200502,
-      'description': 'comprar semilla chicharo',
-      'cost': 2000,
-      'earning': false
-    });
-    await addConcept(admin_token, {
-      'date': 20200503,
-      'description': 'venta de frijol',
-      'cost': 3000,
-      'earning': true
-    });
-    await addConcept(admin_token, {
-      'date': 20200504,
-      'description': 'venta de chicharo',
-      'cost': 4000,
-      'earning': true
-    });
-    await getConceptsReport_test(admin_token, {
       'from': 20200502,
       'to': 20200503
-    }, {
+    }
+  }, function (err, res, body) {
+    assert(JSON.stringify(body).localeCompare(JSON.stringify({
       'concepts': [
         {
           'date': 20200502,
@@ -117,11 +70,25 @@ function runTests(admin_token) {
         }
       ],
       'total': 1000
-    });
-    await getEarningsReport_test(admin_token, {
+    })) == 0);
+  });
+}
+
+function getEarningsReport_test(token) {
+  return request({
+    method: 'GET',
+    json: true,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': token
+    },
+    uri: 'http://localhost:5000/getEarningsReport',
+    body: {
       'from': 20200502,
       'to': 20200503
-    }, {
+    }
+  }, function (err, res, body) {
+    assert(JSON.stringify(body).localeCompare(JSON.stringify({
       'concepts': [
         {
           'date': 20200503,
@@ -131,11 +98,25 @@ function runTests(admin_token) {
         }
       ],
       'total': 3000
-    });
-    await getExpensesReport_test(admin_token, {
+    })) == 0);
+  });
+}
+
+function getExpensesReport_test(token, body, expected_res) {
+  return request({
+    method: 'GET',
+    json: true,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': token
+    },
+    uri: 'http://localhost:5000/getExpensesReport',
+    body: {
       'from': 20200502,
       'to': 20200503
-    }, {
+    }
+  }, function (err, res, body) {
+    assert(JSON.stringify(body).localeCompare(JSON.stringify({
       'concepts': [
         {
           'date': 20200502,
@@ -145,7 +126,16 @@ function runTests(admin_token) {
         }
       ],
       'total': -2000
-    });
+    })) == 0);
+  });
+}
+
+function runTests(admin_token) {
+  return new Promise(async function (resolve, reject) {
+    await addConcepts(admin_token);
+    await getConceptsReport_test(admin_token);
+    await getEarningsReport_test(admin_token);
+    await getExpensesReport_test(admin_token);
     resolve(0);
   });
 }
